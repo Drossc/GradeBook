9d34c72227411caf3e11970ba8e4260dc5ba32b4
Ross Colette <d.ross.colette@gmail.com>
2018-01-15 18:05:06 -0600

    End of chapter changes made

---
+++ GradeBook/Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,50 @@
+﻿//Need set the declare using the test tools assembly
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grades;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void ComputesHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(90, result.HighestGrade);
+        }
+        [TestMethod]
+        public void ComputesLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(10, result.LowestGrade);
+        }
+        [TestMethod]
+        public void ComputesAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics result = book.ComputeStatistics();
+            //Fails test due to lack of precision during testing
+            //Assert.AreEqual(85.16666666666667, result.AverageGrade);
+            //compare using a double delta to have the pass the test
+            Assert.AreEqual(85.16, result.AverageGrade, 0.01);
+        }
+    }
+}
